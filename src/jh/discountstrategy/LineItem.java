c6994e4f9df40f3d389a94b16173888378896c2f
Jennifer <Jennifer@10.30.10.12>
2016-02-26 06:26:55 -0600

    Third Commit- added LineItem, ConsoleOutput, and OutputStrategy classes/Interface. Tried to add validation for 0 and nulls in appropriate methods.  (not sure if how we are supposed to do it?) Added a printReceipt method. Still exploring how to get everything from LineItem to Receipt to Console.

--- src/jh/discountstrategy/LineItem.java
+++ src/jh/discountstrategy/LineItem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jh.discountstrategy;
 
 /**
@@ -10,61 +5,66 @@ package jh.discountstrategy;
  * @author Jennifer
  */
 public class LineItem {
-     private Product product;
+
+    private Product product;
     private int qty;
 
-    public final void setQty(int qty) { 
+    public final void setQty(int qty) {
         if (qty == 0) {
-            System.out.println("LineItem.setProduct method given illegal argument");}
+            System.out.println("LineItem.setProduct method given illegal argument");
+        }
         this.qty = qty;
     }
 
     public final int getQty() {
         return qty;
     }
-    
 
     public LineItem() {
     }
-
-    public LineItem(Product product, int qty) {   
-        this.product = product;
+ public String getLineItemData () {
+    String data = "Prod ID Item Qty\n" + 
+            "------\n"
+            + this.getProduct().getProductId() + " " +  this.getProduct().getProductName(); 
+     return data + " " +  this.getDiscountedSubTotal(); 
+ }
+    public LineItem(String productId, int qty, DBS db) {
+        setProduct(db.FindProductById(productId)); 
         this.qty = qty;
     }
-    
-    public final String getProductID(){
+
+    public final String getProductID() {
         return product.getProductId();
     }
-    
-    public final String getProductName(){
+
+    public final String getProductName() {
         return product.getProductName();
     }
-    
-    public final double getProductPrice(){
+
+    public final double getProductPrice() {
         return product.getUnitCost();
     }
-    
+
 //    public final double getDiscountTotal(int qty){
 //        return product.getDiscountedProductTotal(qty);
 //    }
-    
     public final Product getProduct() {
         return product;
     }
- 
+
     public final void setProduct(Product product) {
-        if(product == null) {
+        if (product == null) {
             System.out.println("LineItem.setProduct method given illegal argument");
         }
         this.product = product;
     }
-    
-    public final double getSubTotal(){
+
+    public final double getSubTotal() {
         return product.getUnitCost() * qty;
     }
-    
-    public final double getDiscountedSubTotal(int qty){
-        return  (product.getUnitCost()* qty) - product.getDiscountedProductTotal(qty);
-        
-    } 
+
+    public final double getDiscountedSubTotal() {
+        return (product.getUnitCost() * qty) - product.getDiscountedProductTotal(qty);
+
+    }
 }
