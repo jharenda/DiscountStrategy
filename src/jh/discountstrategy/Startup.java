155de562af56fb08a825c0db2f5e34aea398f444
Jennifer <Jennifer@10.30.10.12>
2016-02-28 12:35:32 -0600

    Line item and total working. Need to think about different approach to final output (need a strategy), using the endSale method, etc

--- src/jh/discountstrategy/Startup.java
+++ src/jh/discountstrategy/Startup.java
@@ -1,5 +1,6 @@
-
 package jh.discountstrategy;
+
+import java.text.NumberFormat;
 
 /**
  *
@@ -11,14 +12,36 @@ public class Startup {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    // KLUDGE: do configuration here
-    DBS db = new FakeDatabase(); 
-    
-    
-    // Start talking to objects
-    Register reg = new Register(); 
-    reg.startNewSale("100", db);
-    
+     NumberFormat nf = NumberFormat.getCurrencyInstance();
+        // KLUDGE: do configuration here
+        DBS db = new FakeDatabase();
+ 
+        // Start talking to objects
+        Register reg = new Register();
+        reg.startNewSale("100", db);
+        System.out.println(reg.getStoreName());
+        Customer customer = reg.getReceipt().getCustomer();
+        System.out.println( reg.getReceipt().getDate());
+        System.out.println("Customer " + customer.getName() + " found and added to receipt.");
+        reg.addItemsToSale("11", 2);
+        reg.addItemsToSale("12", 2);
+        reg.addItemsToSale("17", 1);
+        LineItem[] items = reg.getReceipt().getLineItems();
+         System.out.println(reg.getReceipt().lineItemHeader());
+        for (LineItem item : items) {
+           
+            System.out.println(item.getLineItemData());
+        }
+      double preTotal = reg.getReceipt().getSubTotalBeforeDiscount()- reg.getReceipt().getTotalDiscount();
+      double finalDiscount = reg.getReceipt().getSubTotalBeforeDiscount();
+        //System.out.println()("Total amount saved: " + (reg.getReceipt().getSubTotalBeforeDiscount()- reg.getReceipt().getTotalDiscount()));
+       System.out.println();
+       
+        System.out.println("Pre- discount total:  " + nf.format((finalDiscount))); 
+        System.out.println("Amount saved:          " +nf.format(preTotal ));
+       
+        System.out.println("Total after discount: " + nf.format((finalDiscount-preTotal)));
+        
     }
-    
+
 }
